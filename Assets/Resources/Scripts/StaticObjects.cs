effc3415010be6381766a1240aea6bf63d4e5bd8
skeleton625 <chaos0824@naver.com>
2019-09-26 16:37:09 +0900

    사용할 오브젝트들을 저장할 정적 객체 스크립트 구현

---
+++ Assets/Resources/Scripts/StaticObjects.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticObjects : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] AttackBallMaterial;
+    private static Material[] outputMaterial;
+
+    void Start()
+    {
+        outputMaterial = new Material[AttackBallMaterial.Length];
+        for (int i = 0; i < AttackBallMaterial.Length; i++)
+            outputMaterial[i] = AttackBallMaterial[i];
+    }
+
+    public static Material getMaterial(int i)
+    {
+        return outputMaterial[i];
+    }
+
+}
