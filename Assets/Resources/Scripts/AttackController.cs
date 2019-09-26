59205f5f75a2ad7a65e2dd9a123e10c4e5abecda
skeleton625 <chaos0824@naver.com>
2019-09-26 16:34:34 +0900

    Player 관련 공격(눈 발사) 컨트롤 스크립트 구현

---
+++ Assets/Resources/Scripts/AttackController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerAttribute playerAtt;
+    [SerializeField]
+    private GameObject AttackBall;
+    [SerializeField]
+    private float attackCycle;
+    private float curAttackTime;
+
+    private bool isAttack = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if(!isAttack)
+        {
+            curAttackTime += Time.deltaTime;
+            if(curAttackTime > attackCycle)
+            {
+                curAttackTime = 0;
+                isAttack = true;
+            }
+        }
+
+        if(Input.GetMouseButton(0) && isAttack)
+        {
+            Attack();
+            isAttack = false;
+        }
+    }
+
+    private void Attack()
+    {
+        Vector3 ballPosition = transform.position + transform.forward * 0.4f + transform.up*0.5f;
+        GameObject _clone = Instantiate(AttackBall, ballPosition, transform.rotation);
+        _clone.GetComponent<MeshRenderer>().material = StaticObjects.getMaterial(playerAtt.getPlayerNumb());
+    }
+}
