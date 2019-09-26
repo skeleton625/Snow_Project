8c3f5da0015e66d453b6314ff2573b4e1794433b
skeleton625 <chaos0824@naver.com>
2019-09-26 16:33:34 +0900

    Player 내부 속성 스크립트 구현

---
+++ Assets/Resources/Scripts/PlayerAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttribute : MonoBehaviour
+{
+    [SerializeField]
+    private float PlayerHealthBar;
+    [SerializeField]
+    private float PlayerAttackDamage;
+    [SerializeField]
+    private int PlayerNumb;
+
+    public void setHeadthBar(float health)
+    {
+        PlayerHealthBar = health;
+    }
+
+    public float getHealthBar()
+    {
+        return PlayerHealthBar;
+    }
+
+    public void setAttackDamage(float attack)
+    {
+        PlayerAttackDamage = attack;
+    }
+
+    public float getAttackDamage()
+    {
+        return PlayerAttackDamage;
+    }
+
+    public void setPlayerNumb(int num)
+    {
+        PlayerNumb = num;
+    }
+
+    public int getPlayerNumb()
+    {
+        return PlayerNumb;
+    }
+}
