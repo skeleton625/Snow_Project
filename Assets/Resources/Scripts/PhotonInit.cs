751265157a8a301bd5c7b62da801384e2cb72751
WaniFi <chaos0824@naver.com>
2019-10-10 23:56:52 +0900

    PUN2에 맞도록 수정

--- Assets/Resources/Scripts/PhotonInit.cs
+++ Assets/Resources/Scripts/PhotonInit.cs
@@ -1,61 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class PhotonInit : Photon.PunBehaviour
+public class PhotonInit : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     private GameObject PlayerModel;
     [SerializeField]
     private Camera MainCamera;
+    [SerializeField]
+    private byte PlayerNumbers;
+    [SerializeField]
+    private Transform[] Beacon;
+
+    private string gameVersion = "1.0";
+    private int PlayerNumber;
 
     void Awake()
     {
-        // PhotonNetwork에 정의된 Snow_Project에 버전별(1.0)로 접근
-        PhotonNetwork.ConnectUsingSettings("Snow_Project 1.0");
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
-    // 게임 네트워크 로비에 입장됐을 때 호출되는 콜백 함수
-    public override void OnJoinedLobby()
+    void Start()
     {
-        Debug.Log("Joined Lobby");
+        OnLogin();
+    }
+
+    public void OnLogin()
+    {
+        bool[] isExist = new bool[PlayerNumbers];
+        Debug.Log(PhotonNetwork.PlayerList.Length);
+        if (PhotonNetwork.PlayerList.Length == 0)
+        {
+            PhotonNetwork.NickName = 0+"";
+            PlayerNumber = 0;
+        }
+        else
+        {
+            
+            foreach (Player player in PhotonNetwork.PlayerList)
+                isExist[int.Parse(player.NickName)] = true;
+            for(int i = 0; i < PlayerNumbers; i++)
+            {
+                if(isExist[i])
+                {
+                    PhotonNetwork.NickName = i + "";
+                    PlayerNumber = i;
+                    break;
+                }
+            }
+        }
+        
+        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 랜덤 룸과 연결하는 함수
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master!!");
         PhotonNetwork.JoinRandomRoom();
     }
 
-    // 게임 네트워크 로비에 입장하지 못했을 경우 호출되는콜백 함수
-    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
-    {
-        Debug.Log("No Room "+codeAndMsg);
-        PhotonNetwork.CreateRoom("GameRoom");
-    }
-
-    // 게임 네트워크에 룸을 생성했을 때, 호출되는 콜백 함수
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("Finish make a Room");
-    }
-
-    // 게임 네트워크 룸에 입장되었을 경우 호출되는 콜백 함수
+    // 룸과 연결되었을 때 출력되는 콜백 함수
     public override void OnJoinedRoom()
     {
-        Debug.Log("Joined room");
-
+        Debug.Log("Joined Room");
         StartCoroutine(CreatePlayer());
     }
 
-    // 게임 네트워크의 세부 사항을  출력하는 GUI 함수
-    private void OnGUI()
+    // 룸 생성 함수
+    private void CreateRoom()
     {
-        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        PhotonNetwork.CreateRoom("room1", new RoomOptions { MaxPlayers = PlayerNumbers });
+        Debug.Log("Room Name :NULL");
+    }
+
+    // 게임 네트워크 로비에 입장하지 못했을 경우 호출되는콜백 함수
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed join room : " + message);
+        this.CreateRoom();
     }
 
     IEnumerator CreatePlayer()
     {
-        GameObject Player = PhotonNetwork.Instantiate(PlayerModel.name, new Vector3(5, 0, 5), Quaternion.identity, 0);
-        Player.GetComponent<PlayerController>().isControllable = true;
-        Player.GetComponent<AttackController>().isControllable = true;
+        Debug.Log(PlayerNumber);
+        GameObject Player = PhotonNetwork.Instantiate(PlayerModel.name, Beacon[PlayerNumber].position, Quaternion.identity, 0);
+        Player.name = PhotonNetwork.NickName;
         Player.GetComponent<PlayerController>().PlayerCamera = MainCamera;
+        Player.GetComponent<PlayerAttribute>().setPlayerNumb(PlayerNumber);
         yield return null;
     }
 }
