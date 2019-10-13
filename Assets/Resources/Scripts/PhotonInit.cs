61d56486a52b0f33d7909247b27e11f2ec202995
skeleton625 <chaos0824@naver.com>
2019-10-13 20:18:59 +0900

    Player의 NickName 설정 과정 추가

--- Assets/Resources/Scripts/PhotonInit.cs
+++ Assets/Resources/Scripts/PhotonInit.cs
@@ -14,9 +14,11 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     private byte PlayerNumbers;
     [SerializeField]
     private Transform[] Beacon;
+    [SerializeField]
+    private string gameVersion;
 
-    private string gameVersion = "1.0";
-    private int PlayerNumber;
+    private PhotonView pv;
+    private string createdPlayerName;
 
     void Awake()
     {
@@ -25,9 +27,12 @@ public class PhotonInit : MonoBehaviourPunCallbacks
 
     void Start()
     {
+        // SettingPlayerModelName 함수를 위한 PhotonView 컴포넌트
+        pv = GetComponent<PhotonView>();
         OnLogin();
     }
 
+    // Player가 처음 입장했을 때, 초기화
     public void OnLogin()
     {
         PhotonNetwork.NickName = "0";
@@ -39,67 +44,71 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to Master!!");
+        // 방이 존재할 경우 존재하는 방 중 하나를 들어감
         PhotonNetwork.JoinRandomRoom();
     }
 
     // 룸과 연결되었을 때 출력되는 콜백 함수
     public override void OnJoinedRoom()
     {
-        if(PhotonNetwork.NickName != "1")
+        Debug.Log("Joined Room, Generate Player Number");
+        // 들어온 Player의 번호를 매겨주는 함수
+        if (!PhotonNetwork.IsMasterClient)
         {
-            bool[] isExist = new bool[PlayerNumbers + 1];
-            for (int i = 1; i <= PhotonNetwork.PlayerList.Length-1; i++)
-            {
-                isExist[int.Parse(PhotonNetwork.PlayerList[i].NickName)] = true;
-            }
+            bool[] isExist = new bool[PlayerNumbers];
+            foreach(Player p in PhotonNetwork.PlayerList)
+                isExist[int.Parse(p.NickName)] = true;
                 
-            for (int i = 1; i <= PlayerNumbers; i++)
+            for (int i = 0; i < PlayerNumbers; i++)
             {
                 if (!isExist[i])
                 {
                     PhotonNetwork.NickName = i + "";
-                    PlayerNumber = i;
                     break;
                 }
             }
         }
-        Debug.Log("Joined Room");
         StartCoroutine(CreatePlayer());
     }
 
+    // Player가 들어왔을 때 알려주는 함수
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        Debug.Log("new Player : " + newPlayer.NickName);
+        Debug.Log("here");
     }
 
+    // Player가 나갔을 때 알려주는 함수
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        foreach (Player p in PhotonNetwork.PlayerList)
-            Debug.Log(p.NickName);
         Debug.Log("Left Player : " + otherPlayer.NickName);
     }
 
     // 게임 네트워크 로비에 입장하지 못했을 경우 호출되는콜백 함수
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
+        // 테스트 상황에서 가장 첫 번째 참가자
         Debug.Log("Failed join room : " + message);
-        PhotonNetwork.NickName = "1";
         this.CreateRoom();
     }
 
     // 룸 생성 함수
     private void CreateRoom()
     {
+        // "room1"이란 이름으로 방을 생성
         PhotonNetwork.CreateRoom("room1", new RoomOptions { MaxPlayers = PlayerNumbers });
-        Debug.Log("Room Name :room1");
     }
 
     IEnumerator CreatePlayer()
     {
-        GameObject Player = PhotonNetwork.Instantiate(PlayerModel.name, Beacon[PlayerNumber].position, Quaternion.identity, 0);
+        // 캐릭터 모델을 복사해 Player 캐릭터 생성
+        GameObject Player = PhotonNetwork.Instantiate(PlayerModel.name, 
+                                                        Beacon[int.Parse(PhotonNetwork.NickName)].position, 
+                                                        Quaternion.identity, 0);
+        // Player 캐릭터의 세부 사항 설정
         Player.name = PhotonNetwork.NickName;
         Player.GetComponent<PlayerController>().PlayerCamera = MainCamera;
-        Player.GetComponent<PlayerAttribute>().setPlayerNumb(PlayerNumber);
+        Player.GetComponent<PlayerAttribute>().setAttackDamage(5);
+        Player.GetComponent<PlayerAttribute>().setPlayerNumb(int.Parse(PhotonNetwork.NickName));
         yield return null;
     }
 }
