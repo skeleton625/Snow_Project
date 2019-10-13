31bfebf86b6da3ed4f92e86906da8c720e9dd43c
skeleton625 <chaos0824@naver.com>
2019-10-13 20:08:30 +0900

    한 Player의 공격이 다른 Player에게 피해를 입힐 수 있도록 충돌 처리 함수 수정(2명 한정)

--- Assets/Resources/Scripts/BallController.cs
+++ Assets/Resources/Scripts/BallController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class BallController : MonoBehaviourPunCallbacks, IPunObservable
+public class BallController : MonoBehaviourPunCallbacks
 {
+    // 날아가는 공의 속도
     [SerializeField]
     private float ballSpeed;
+    // 공이 생성되어 날아가는 동안의 시간
     [SerializeField]
     private float limitTime;
     private float curTime;
 
-    private GameObject AttackedPlayer;
-    private int isAttacked;
+    // 공의 피해량
     public float ballDamage;
+    // 공을 던진 Player의 번호
     public int ThrowPlayer;
 
     // Update is called once per frame
@@ -22,47 +24,37 @@ public class BallController : MonoBehaviourPunCallbacks, IPunObservable
         ThrowBall();
     }
 
+    // 공을 던지는 함수
     private void ThrowBall()
     {
+        // 제한 시간 동안 공이 움직이도록 함
         if (curTime < limitTime)
         {
             curTime += Time.deltaTime;
             transform.Translate(Vector3.forward * ballSpeed * Time.deltaTime);
         }
+        // 제한 시간이 지나면 공을 삭제
         else
            Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        // 공과 충돌한 Player의 번호를 파악
         string objectName = collision.gameObject.name;
-        if(objectName != "SnowBall(Clone)" && objectName != "Wall" && objectName != (ThrowPlayer+""))
-        {
-            AttackedPlayer = collision.gameObject;
-            isAttacked = 1;
-        }
+        // 충돌한 Player의 PhotonView 가져옴
+        PhotonView pv = GameObject.Find(objectName+"").GetComponent<PhotonView>();
+        if(objectName != "SnowBall(Clone)" && objectName != "Wall")
+            pv.RPC("AttackingPlayer", RpcTarget.All, int.Parse(objectName), ballDamage);
+        // 충돌된 경우 공을 삭제
         Destroy(gameObject);
     }
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    [PunRPC]
+    public void AttackingPlayer(int PlayerNumber, float PlayerDamage)
     {
-        Debug.Log(isAttacked);
-        if (stream.IsWriting && isAttacked == 1)
-        {
-            stream.SendNext(ThrowPlayer);
-            stream.SendNext(ballDamage);
-            stream.SendNext(AttackedPlayer);
-            isAttacked = 2;
-        }
-        else if(isAttacked == 2)
-        {
-            int AttackPlayer = (int) stream.ReceiveNext();
-            float AttackDamage = (float) stream.ReceiveNext();
-            GameObject AttackedPlayer = (GameObject)stream.ReceiveNext();
-            AttackedPlayer.GetComponent<PlayerAttribute>().setHealthBar(AttackDamage);
-            Debug.Log(AttackedPlayer.GetComponent<PlayerAttribute>().getHealthBar());
-            isAttacked = 0;
-        }
+        GameObject AttackedPlayer = GameObject.Find(PlayerNumber.ToString());
+        Debug.Log(PlayerNumber + " " + PlayerDamage);
+        AttackedPlayer.GetComponent<PlayerAttribute>().setHealthBar(PlayerDamage);
     }
 }
