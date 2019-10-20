5cd70df32abc9f1f7c2d9c0e077b4c0cfbce13cc
WaniFi <chaos0824@naver.com>
2019-10-20 23:49:26 +0900

    Menu Scene의 Button들을 관리하는 Script

---
+++ Assets/Resources/Scripts/MenuScript/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private MenuManager PlayerMenu;
+
+    void Start()
+    {
+        PlayerMenu = GameObject.Find("PhotonNetworkObject").GetComponent<MenuManager>();    
+    }
+
+    public void JoinLobbyButtonClick()
+    {
+        string _playerName = GameObject.Find("UserName/InputField").GetComponent<InputField>().text;
+        PlayerMenu.setPlayerName(_playerName);
+        PlayerMenu.setMenuActive(1);
+    }
+
+    public void RoomButtonClick()
+    {
+        PlayerMenu.setMenuActive(2);
+    }
+
+    public void JoinRoomButtonClick()
+    {
+        PlayerMenu.PlayerJoinRoom(gameObject.name);
+        PlayerMenu.setMenuActive(3);
+    }
+
+    public void CreateRoomButtonClick()
+    {
+        string _roomName = GameObject.Find("RoomName/InputField").GetComponent<InputField>().text;
+        PlayerMenu.CreateRoomByMaster(_roomName);
+        PlayerMenu.setMenuActive(3);
+    }
+
+    public void CancelRoomButtonClick()
+    {
+        PlayerMenu.setMenuActive(1);
+    }
+
+    public void LeaveRoomButtonClick()
+    {
+        PlayerMenu.PlayerLeaveRoom();
+        PlayerMenu.setMenuActive(1);
+    }
+}
