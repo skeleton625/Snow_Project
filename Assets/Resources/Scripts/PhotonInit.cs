9f510befbdab62597b1e49b4906f5c0dcfce14f2
skeleton625 <chaos0824@naver.com>
2019-10-30 16:54:37 +0900

    게임 씬으로 이동한 뒤, 생성된 Player Object에 번호를 표시하는 기능 추가 구현

--- Assets/Resources/Scripts/PhotonInit.cs
+++ Assets/Resources/Scripts/PhotonInit.cs
@@ -34,17 +34,37 @@ public class PhotonInit : MonoBehaviourPunCallbacks
         StartCoroutine(CreatePlayer());
     }
 
-    IEnumerator CreatePlayer()
+    private IEnumerator CreatePlayer()
     {
         // 캐릭터 모델을 복사해 Player 캐릭터 생성
         GameObject _player = PhotonNetwork.Instantiate(PlayerModel.name, 
                                                         Beacon[MasterPlayerPos].position, 
                                                         Quaternion.identity, 0);
         // Player 캐릭터의 세부 사항 설정
-        _player.name = MasterPlayerPos.ToString();
         _player.GetComponent<PlayerController>().PlayerCamera = MainCamera;
         _player.GetComponent<PlayerAttribute>().setAttackDamage(5);
         _player.GetComponent<PlayerAttribute>().setPlayerNumb(MasterPlayerPos);
+        StartCoroutine(SettingPlayerNames());
+        yield return null;
+    }
+
+    private IEnumerator SettingPlayerNames()
+    {
+        float timer = 0;
+        RaycastHit Players;
+        while(timer < 3)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                Vector3 _rayPos = Beacon[i].position + new Vector3(0, 2, 0);
+                Physics.Raycast(_rayPos, new Vector3(0, -1, 0), out Players, 2f);
+
+                if(Players.point != Vector3.zero)
+                    Players.transform.gameObject.name = "" + i;
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
         yield return null;
     }
 }
