499a9bf4aca17bf5520ea21f0aed8bc00d15997b
skeleton625 <chaos0824@naver.com>
2019-10-30 16:52:47 +0900

    이전과 동일

---
+++ Assets/Resources/Scripts/MouseManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLock(false);
+    }
+
+    private void MouseLock(bool _isVisible)
+    {
+        Cursor.visible = _isVisible;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
