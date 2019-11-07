f6c21a4acac09d1cdca5c4180964be166fbc6ddc
skeleton625 <chaos0824@naver.com>
2019-11-07 18:22:38 +0900

    Player 죽음에 대한 함수 구현 중

--- Assets/Resources/Scripts/InGameScript/PhotonInGame.cs
+++ Assets/Resources/Scripts/InGameScript/PhotonInGame.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class PhotonInGame : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    private GameObject PlayerModel;
+    private string PlayerModelPos;
     [SerializeField]
-    private GameObject PlayerDeadModel;
+    private string PlayerDeadModelPos;
+    [SerializeField]
+    private string PlayerDeadEffectPos;
     [SerializeField]
     private int PlayerNumbers;
     [SerializeField]
@@ -20,11 +19,11 @@ public class PhotonInGame : MonoBehaviourPunCallbacks
     [SerializeField]
     private float ResponTime;
 
-    private float PlayerDeadTime;
     private int MasterPlayerPos;
+    private float PlayerDeadTime;
     private GameObject MasterPlayer;
     private PhotonView pv;
-    private bool[] BeaconPosition;
+    private bool IsDead;
 
     void Awake()
     {
@@ -34,57 +33,77 @@ public class PhotonInGame : MonoBehaviourPunCallbacks
     void Start()
     {
         // SettingPlayerModelName 함수를 위한 PhotonView 컴포넌트
-        pv = GetComponent<PhotonView>();
-        BeaconPosition = new bool[Beacon.Length];
-        //MasterPlayerPos = int.Parse(PhotonNetwork.NickName.Split('_')[1]);
-        //StartCoroutine(CreatePlayer());
+        pv = gameObject.GetComponent<PhotonView>();
+        MasterPlayerPos = int.Parse(PhotonNetwork.NickName.Split('_')[1]);
+        StartCoroutine(CreatePlayer());
     }
 
     void Update()
     {
-        if(MasterPlayer != null && MasterPlayer.GetComponent<PlayerAttribute>().getPlayerDead())
-        {
+        if (MasterPlayer != null)
             PlayerDead();
+    }
+
+    void LateUpdate()
+    {
+        if (IsDead)
+        {
+            PlayerDelete(MasterPlayerPos + "", MasterPlayer.transform.position);
+            IsDead = false;
         }
+            
     }
 
     private void PlayerDead()
     {
         if (MasterPlayer.GetComponent<PlayerAttribute>().getPlayerDead())
         {
-
-            GameObject _deadClone = PhotonNetwork.Instantiate(PlayerDeadModel.name,
-                                                        MasterPlayer.transform.position,
-                                                        Quaternion.identity, 0);
-            Destroy(_deadClone);
+            Vector3 _deadPos = MasterPlayer.transform.position;
             MainCamera.transform.parent = null;
-            _deadClone.name = MasterPlayerPos + "";
-            StartCoroutine(PlayerDeadMotion(_deadClone));
+            MainCamera.transform.position = _deadPos + new Vector3(0, 1.5f, 0);
+            
+            pv.RPC("PlayerDelete", RpcTarget.Others, MasterPlayerPos+"", _deadPos);
+            MasterPlayer.GetComponent<PlayerAttribute>().setPlayerDead(false);
+            IsDead = true;
         }
     }
-
-    private IEnumerator PlayerDeadMotion(GameObject _deadModel)
+    
+    [PunRPC]
+    private void PlayerDelete(string _playerName, Vector3 _deadPos)
     {
+        StartCoroutine(PlayerDeadMotion(_deadPos));
+        Destroy(GameObject.Find(_playerName));
+    }
+
+    private IEnumerator PlayerDeadMotion(Vector3 _deadPos)
+    {
+        GameObject _deadClone = Instantiate(Resources.Load(PlayerDeadModelPos) as GameObject
+                                            , _deadPos, Quaternion.identity);
         float _xRot = 0;
+
         while (_xRot > -90)
         {
             _xRot = Mathf.Lerp(_xRot, -91, 0.1f);
-            _deadModel.transform.localEulerAngles = new Vector3(_xRot, 0, 0);
+            _deadClone.transform.localEulerAngles = new Vector3(_xRot, 0, 0);
             yield return null;
         }
-        Destroy(_deadModel, 1f);
+
+        GameObject _deadEffect = Instantiate(Resources.Load(PlayerDeadEffectPos) as GameObject
+                                             , _deadPos, Quaternion.identity);
+        Destroy(_deadClone, 0.5f);
+        Destroy(_deadEffect, 3f);
         yield return null;
     }
 
     private IEnumerator CreatePlayer()
     {
         // 캐릭터 모델을 복사해 Player 캐릭터 생성
-        GameObject _player = PhotonNetwork.Instantiate(PlayerModel.name, 
+        GameObject _player = PhotonNetwork.Instantiate(PlayerModelPos, 
                                                         Beacon[MasterPlayerPos].position, 
                                                         Quaternion.identity, 0);
         // Player 캐릭터의 세부 사항 설정
         _player.GetComponent<PlayerController>().PlayerCamera = MainCamera;
-        _player.GetComponent<PlayerAttribute>().setAttackDamage(5);
+        _player.GetComponent<PlayerAttribute>().setAttackDamage(100);
         _player.GetComponent<PlayerAttribute>().setPlayerNumb(MasterPlayerPos);
         MasterPlayer = _player;
         StartCoroutine(SettingPlayerNames());
