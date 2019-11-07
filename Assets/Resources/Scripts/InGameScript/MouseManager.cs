d30dba5b6dd19672241b76095724a07d84c5a5ac
skeleton625 <chaos0824@naver.com>
2019-11-07 18:29:18 +0900

    MouseManager.cs 파일 위치 수정

---
+++ Assets/Resources/Scripts/InGameScript/MouseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    private bool IsVisible;
+
+    void Start()
+    {
+        IsVisible = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLockInScene();
+    }
+
+    private void MouseLockInScene()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsVisible = !IsVisible;
+
+            Cursor.visible = IsVisible;
+            if (IsVisible)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }        
+    }
+}
