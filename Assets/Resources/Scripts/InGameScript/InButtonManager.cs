4d22cd887b394e64f0bca0e4583768f6bf79eabf
skeleton625 <chaos0824@naver.com>
2019-11-11 21:15:12 +0900

    실제 Game Scene 내 버튼 관리 스크립트 추가

---
+++ Assets/Resources/Scripts/InGameScript/InButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+public class InButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void ReturnToLobbyButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
