f0c4d6a4bfc46b755df64e1adabaf73eb2eb6bb9
skeleton625 <chaos0824@naver.com>
2019-11-13 21:57:18 +0900

    InGame 내에 존재하게 되는 플레이어의 정보를 저장하는 스크립트 구현

---
+++ Assets/Resources/Scripts/StaticObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticObjects
+{
+    private static bool[] playerExist = new bool[4];
+
+    public static void SetPlayerExist(int _num)
+    {
+        playerExist[_num] = true;
+    }
+
+    public static bool GetPlayerExist(int _num)
+    {
+        return playerExist[_num];
+    }
+
+    public static void InitPlayerExist()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            playerExist[i] = false;
+        }
+    }
+}
