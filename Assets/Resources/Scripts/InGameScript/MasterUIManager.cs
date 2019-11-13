3c900081c5fdafccc6fc2a8594ea14efaf4e1d0b
skeleton625 <chaos0824@naver.com>
2019-11-13 21:56:42 +0900

    이전 UIController 스크립트 리펙토링 중

---
+++ Assets/Resources/Scripts/InGameScript/MasterUIManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MasterUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject CountScene;
+    [SerializeField]
+    private GameObject DeadText;
+    [SerializeField]
+    private GameObject CountText;
+    [SerializeField]
+    private GameObject ResumeScene;
+    [SerializeField]
+    private GameObject HealthBar;
+
+    private int MasterPlayerNum;
+    private UIController MasterUI;
+    private bool MouseVisible = true;
+
+    void Start()
+    {
+        MasterPlayerNum = 
+            GameObject.Find("StaticObjects").GetComponent<PlayerManager>().MasterPlayerNum;
+        MasterUI = new UIController(GameObject.Find(MasterPlayerNum+""), HealthBar);
+
+        MouseVisible = false;
+        Cursor.visible = MouseVisible;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLockInScene();
+        MasterUI.SetPlayerHealthBar();
+    }
+
+    private void MouseLockInScene()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MouseVisible = !MouseVisible;
+
+            Cursor.visible = MouseVisible;
+            if (MouseVisible)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+
+            ResumeScene.SetActive(MouseVisible);
+        }
+    }
+
+    public void VisibleCountScene(float _cnt, bool _isDead)
+    {
+        StartCoroutine(CountdownCoroutine(_cnt, _isDead));
+    }
+
+    private IEnumerator CountdownCoroutine(float _cnt, bool _isDead)
+    {
+        if (_isDead)
+            DeadText.SetActive(true);
+
+        CountScene.SetActive(true);
+
+        yield return new WaitForSeconds(_cnt);
+
+        CountScene.SetActive(false);
+
+        if (_isDead)
+            DeadText.SetActive(false);
+    }
+}
--- Assets/Resources/Scripts/InGameScript/UIController.cs
+++ Assets/Resources/Scripts/InGameScript/UIController.cs
@@ -1,65 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using Photon.Pun;
 using System.Collections;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject PlayerUI;
+    private GameObject PlayerModel;
+    [SerializeField]
+    private GameObject HealthUI;
 
-    private Slider HealthBar;
-    private PhotonView pv;
-    private PlayerAttribute PlayerAtt;
-    private Transform TargetCamera;
     private float PrePlayerHealth;
+    private Slider HealthBar;
+    private PlayerAttribute PlayerAtt;
 
-    private GameObject DeadScene, ResumeScene;
-
-    private bool MouseVisible;
-
-    void Start()
+    public UIController()
     {
-        pv = GetComponent<PhotonView>();
-        PlayerAtt = GetComponent<PlayerAttribute>();
-        DeadScene = GameObject.Find("CharacterUI").transform.GetChild(0).gameObject;
-        ResumeScene = GameObject.Find("CharacterUI").transform.GetChild(1).gameObject;
-        InitCharacterUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public UIController(GameObject _player, GameObject _healthUI)
     {
-        if (!pv.IsMine)
-            HealthBar.transform.LookAt(TargetCamera);
-        SetPlayerHealthBar();
-        MouseLockInScene();
-    }
+        HealthUI.SetActive(false);
 
-    private void InitMouseVisible()
-    {
-        MouseVisible = false;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        HealthUI = _healthUI;
+        PlayerModel = _player;
 
-    private void InitCharacterUI()
-    {
-        TargetCamera = GameObject.Find("MainCamera").transform;
-        if (pv.IsMine)
-        {
-            HealthBar = GameObject.Find("CharacterUI/MainHealthBar").GetComponent<Slider>();
-            Destroy(PlayerUI);
-        }
-        else
-            HealthBar = PlayerUI.transform.Find("HealthBar").GetComponent<Slider>();
+        PlayerAtt = _player.GetComponent<PlayerAttribute>();
+        HealthBar = _healthUI.GetComponent<Slider>();
 
         PrePlayerHealth = PlayerAtt.getHealthBar();
         HealthBar.value = PrePlayerHealth / PlayerAtt.getHealthBarMax();
-        PlayerUI.SetActive(false);
     }
 
-    private void SetPlayerHealthBar()
+    public void SetPlayerHealthBar()
     {
         if(PlayerAtt.getHealthBar() != PrePlayerHealth)
         {
@@ -67,42 +39,21 @@ public class UIController : MonoBehaviour
             HealthBar.value = PrePlayerHealth / PlayerAtt.getHealthBarMax();
         }
     }
-
-    private IEnumerator ActivateHealthBar()
-    {
-        PlayerUI.SetActive(true);
-
-        yield return new WaitForSeconds(3f);
-
-        PlayerUI.SetActive(false);
-
-        yield return null;
-    }
-
-    private void MouseLockInScene()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            MouseVisible = !MouseVisible;
-
-            Cursor.visible = MouseVisible;
-            if (MouseVisible)
-                Cursor.lockState = CursorLockMode.None;
-            else
-                Cursor.lockState = CursorLockMode.Locked;
-
-            ResumeScene.SetActive(MouseVisible);
-        }
-    }
-
     public void VisibleHealthBar()
     {
         StopCoroutine(ActivateHealthBar());
+        SetPlayerHealthBar();
         StartCoroutine(ActivateHealthBar());
     }
 
-    public void VisibleDeadScene()
+    private IEnumerator ActivateHealthBar()
     {
-        DeadScene.SetActive(true);
+        HealthUI.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
+
+        HealthUI.SetActive(false);
+
+        yield return null;
     }
 }
