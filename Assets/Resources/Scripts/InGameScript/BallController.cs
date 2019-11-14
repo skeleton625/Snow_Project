ced89d2cf6b90b11223520d2b65f2b4894412624
skeleton625 <chaos0824@naver.com>
2019-11-14 21:05:03 +0900

    ThorwPlayer, BallDamage 변수 리펙토링

--- Assets/Resources/Scripts/InGameScript/BallController.cs
+++ Assets/Resources/Scripts/InGameScript/BallController.cs
@@ -5,27 +5,24 @@ using Photon.Pun;
 
 public class BallController : MonoBehaviourPunCallbacks
 {
-    // 날아가는 공의 속도
+    // 공의 속성 값
     [SerializeField]
     private float ballSpeed;
     [SerializeField]
     private float ballForce;
-    // 공이 생성되어 날아가는 동안의 시간
+    private float ballDamage;
+    public float BallDamage
+        { set { ballDamage = value; } }
+    private int throwPlayerNum;
+    public int ThrowPlayerNum
+        { set { throwPlayerNum = value; } }
+
     [SerializeField]
     private float limitTime;
     private float curTime;
-    // 충돌 이펙트 오브젝트
+
+    [SerializeField]
     private GameObject hitEffect;
-
-    // 공의 피해량
-    public float ballDamage;
-    // 공을 던진 Player의 번호
-    public int ThrowPlayer;
-
-    void Start()
-    {
-        hitEffect = Resources.Load("Prefabs/Particle/hitEffect") as GameObject;
-    }
 
     // Update is called once per frame
     void Update()
@@ -44,7 +41,7 @@ public class BallController : MonoBehaviourPunCallbacks
         }
         // 제한 시간이 지나면 공을 삭제
         else
-           Destroy(gameObject);
+            Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -52,7 +49,7 @@ public class BallController : MonoBehaviourPunCallbacks
         int _parsedName;
         // 공과 충돌한 Player의 번호를 파악
         string _objectName = collision.gameObject.name;
-        if (_objectName == ThrowPlayer+"")
+        if (_objectName == throwPlayerNum+"")
             return;
 
         Vector3 conflictPos = collision.contacts[0].point;
@@ -68,15 +65,12 @@ public class BallController : MonoBehaviourPunCallbacks
 
             // 충돌한 Player의 피해를 다른 플레이어들에게도 갱신해 줌
             PhotonView pv = GameObject.Find(_objectName).GetComponent<PhotonView>();
-            pv.RPC("AttackingPlayer", RpcTarget.All, _parsedName, ballDamage);
+            pv.RPC("AttackingPlayer", RpcTarget.All, _parsedName, ballDamage, gameObject);
 
             // 충돌한 Player의 Rigidbody를 통해 넉백을 진행
             Vector3 _knockBack = collision.contacts[0].point.normalized + new Vector3(0, 1f, 0);
             collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             collision.gameObject.GetComponent<Rigidbody>().AddForce(_knockBack * ballForce);
         }
-            
-        // 충돌된 경우 공을 삭제
-        Destroy(gameObject);
     }
 }
