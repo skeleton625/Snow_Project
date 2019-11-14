9d52288359e4eb95ba7318b84f88f11ecd962c3d
skeleton625 <chaos0824@naver.com>
2019-11-14 21:13:06 +0900

    게임 내에서 사용하는 각 오브젝트 및 모델들에 대한 연결 추가

--- Assets/Resources/Scripts/StaticObjects.cs
+++ Assets/Resources/Scripts/StaticObjects.cs
@@ -1,10 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public static class StaticObjects
+public class StaticObjects : MonoBehaviour
 {
+    private static int ballCount;
+    public static int BallCount
+    {
+        get { return ballCount; }
+        set { ballCount += value; }
+    }
+    private static int masterPlayerNumber;
+    public static int MasterPlayerNumber
+    {
+        get{ return masterPlayerNumber; }
+        set{ masterPlayerNumber = value; }
+    }
+    private static Player[] GamePlayerList;
     private static bool[] playerExist = new bool[4];
+    [SerializeField]
+    private Transform[] Beacons;
+    [SerializeField]
+    private GameObject[] Players;
+    [SerializeField]
+    private GameObject[] PlayerDeads;
+
+    public Transform GetBeacons(int _num)
+    {
+        return Beacons[_num];
+    }
+
+    public GameObject GetPlayerModels(int _num)
+    {
+        return Players[_num];
+    }
+
+    public GameObject GetPlayerDeads(int _num)
+    {
+        return PlayerDeads[_num];
+    }
 
     public static void SetPlayerExist(int _num)
     {
@@ -22,5 +58,15 @@ public static class StaticObjects
         {
             playerExist[i] = false;
         }
+    }
+
+    public static void SetPlayerList(Player[] _playerlist)
+    {
+        GamePlayerList = _playerlist;
+    }
+
+    public static Player GetPlayer(int _num)
+    {
+        return GamePlayerList[_num];
     }
 }
