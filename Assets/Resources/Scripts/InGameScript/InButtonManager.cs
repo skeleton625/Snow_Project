a5e999b783aca75a3a020ddf4e1ec432b86d82ac
skeleton625 <chaos0824@naver.com>
2019-11-14 21:06:04 +0900

    게임 중, 게임을 나갈 시 해당 모델을 비활성화 상태로 변경하도록 수정

--- Assets/Resources/Scripts/InGameScript/InButtonManager.cs
+++ Assets/Resources/Scripts/InGameScript/InButtonManager.cs
@@ -3,14 +3,18 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 public class InButtonManager : MonoBehaviour
 {
+    private PlayerManager PManager;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        PManager = GameObject.Find("StaticObjects").GetComponent<PlayerManager>();
     }
+
     public void ReturnToLobbyButtonClick()
     {
         StaticObjects.InitPlayerExist();
+        PManager.PlayerOutTheGame(StaticObjects.MasterPlayerNumber);
         SceneManager.LoadScene("MainMenu");
     }
 }
