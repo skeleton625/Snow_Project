14de56c50a2ef7aec04e4b9839076ffc0d7886dd
skeleton625 <chaos0824@naver.com>
2019-11-14 21:07:31 +0900

    PlayerAtribute.cs 스크립트 리펙토링

--- Assets/Resources/Scripts/InGameScript/PlayerAttribute.cs
+++ Assets/Resources/Scripts/InGameScript/PlayerAttribute.cs
@@ -2,56 +2,44 @@
 
 public class PlayerAttribute : MonoBehaviour
 {
-    // 현재 Player의 체력
-    [SerializeField]
-    private float PlayerHealthBar;
-    // 현재 Player의 공격력
-    [SerializeField]
-    private float PlayerAttackDamage;
     // 현재 Player의 번호
     [SerializeField]
-    private int PlayerNumb;
-
+    private int playerNumber;
+    public int PlayerNumber
+    {
+        get{ return playerNumber; }
+        set{ playerNumber = value; }
+    }
+    
+    // 현재 Player의 공격력
+    [SerializeField]
+    private float playerDamage;
+    public float PlayerDamage
+    {
+        get { return playerDamage; }
+        set { playerDamage = value; }
+    }
     // Player의 최대 체력
-    private float PlayerHealthBarMax;
+    [SerializeField]
+    private float playerHealthMax;
+    public float PlayerHealthMax
+    {
+        get { return playerHealthMax; }
+    }
+
+    private float playerHealth;
+    public float PlayerHealth
+    {
+        get { return playerHealth; }
+        set
+        {
+            if(playerHealth > 0)
+                playerHealth -= value;
+        }
+    }
 
     void Start()
     {
-        PlayerHealthBarMax = PlayerHealthBar;
-    }
-
-    public void setHealthBar(float health)
-    {
-        PlayerHealthBar -= health;
-    }
-
-    public float getHealthBar()
-    {
-        return PlayerHealthBar;
-    }
-
-    public float getHealthBarMax()
-    {
-        return PlayerHealthBarMax;
-    }
-
-    public void setAttackDamage(float attack)
-    {
-        PlayerAttackDamage = attack;
-    }
-
-    public float getAttackDamage()
-    {
-        return PlayerAttackDamage;
-    }
-
-    public void setPlayerNumb(int num)
-    {
-        PlayerNumb = num;
-    }
-
-    public int getPlayerNumb()
-    {
-        return PlayerNumb;
+        playerHealth = playerHealthMax;    
     }
 }
