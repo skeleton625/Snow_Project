6ef91d24a027458cdde76c05482fb1824f768292
skeleton625 <chaos0824@naver.com>
2019-11-19 23:39:24 +0900

    KillingList 내애 들어가는 KillBlock 스크립트 정의

---
+++ Assets/Resources/Scripts/InGameScript/KillBlock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KillBlock : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject KillerPlayer;
+    [SerializeField]
+    private GameObject KilledPlayer;
+
+    public void SetPlayerNames(string _killedName, string _killerName)
+    {
+        KillerPlayer.GetComponent<Text>().text = _killedName;
+        KilledPlayer.GetComponent<Text>().text = _killerName;
+    }
+}
