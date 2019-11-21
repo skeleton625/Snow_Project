07200d4e65b8a6dcc3e5c2c56c1c66f3915da755
skeleton625 <chaos0824@naver.com>
2019-11-21 17:08:07 +0900

    게임 시간 측정 함수 및 게임 종료 후, PlayerRanK 표현 함수 구현

--- Assets/Resources/Scripts/InGameScript/MasterUIManager.cs
+++ Assets/Resources/Scripts/InGameScript/MasterUIManager.cs
@@ -1,28 +1,42 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MasterUIManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject CountScene;
+    private Text GameTimer;
     [SerializeField]
     private GameObject DeadText;
     [SerializeField]
     private GameObject CountText;
     [SerializeField]
-    private GameObject ResumeScene;
+    private GameObject TimerLayer;
+    [SerializeField]
+    private GameObject GameSetLayer;
+    [SerializeField]
+    private Text[] PlayerRank;
+    [SerializeField]
+    private GameObject CountLayer;
+    [SerializeField]
+    private GameObject ResumeLayer;
+    [SerializeField]
+    private GameObject MainHealthBar;
 
+    private int CurrentTime;
     private bool isMouseVisible = true;
     public bool IsMouseVisible
         { get { return isMouseVisible; } }
+    private bool IsGameEnd;
 
     void Start()
     {
+        CurrentTime = StaticObjects.GamePlayTime;
         InGameObjects staticObject = 
             GameObject.Find("StaticObjects").GetComponent<InGameObjects>();
         GameObject model = staticObject.GetPlayerModels(StaticObjects.MasterPlayerNumber);
-        StartCoroutine(ActivateCountScene(3, model));
 
         // 마우스 초기화
         Cursor.visible = isMouseVisible;
@@ -37,7 +51,7 @@ public class MasterUIManager : MonoBehaviour
 
     private void MouseLockInScene()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !IsGameEnd)
         {
             isMouseVisible = !isMouseVisible;
 
@@ -47,14 +61,14 @@ public class MasterUIManager : MonoBehaviour
             else
                 Cursor.lockState = CursorLockMode.Locked;
 
-            ResumeScene.SetActive(isMouseVisible);
+            ResumeLayer.SetActive(isMouseVisible);
         }
     }
 
     public IEnumerator ActivateDeadScene(float _cnt)
     {
         DeadText.SetActive(true);
-        CountScene.SetActive(true);
+        CountLayer.SetActive(true);
         while (_cnt > 0)
         {
             CountText.GetComponent<Text>().text = _cnt + "";
@@ -62,12 +76,13 @@ public class MasterUIManager : MonoBehaviour
             yield return new WaitForSeconds(1f);
             --_cnt;
         }
-        CountScene.SetActive(false);
+        CountLayer.SetActive(false);
         DeadText.SetActive(false);
     }
 
-    private IEnumerator ActivateCountScene(float _cnt, GameObject _model)
+    public IEnumerator ActivateGameStart(float _cnt)
     {
+        GameTimer.GetComponent<Text>().text = CurrentTime / 60 + " : " + CurrentTime % 60;
         while (_cnt > 0)
         {
             CountText.GetComponent<Text>().text = _cnt + "";
@@ -76,6 +91,48 @@ public class MasterUIManager : MonoBehaviour
             --_cnt;
         }
         isMouseVisible = false;
-        CountScene.SetActive(false);
+        CountLayer.SetActive(false);
+    }
+
+    public IEnumerator ClockingGameTimeCoroutine()
+    {
+        Text _timer = GameTimer.GetComponent<Text>();
+        int _sec = CurrentTime % 60;
+        int _min = CurrentTime / 60;
+        _timer.text = _min + " : " + _sec;
+        while (_min >= 0)
+        {
+            yield return new WaitForSeconds(1f);
+            --_sec;
+
+            if (_sec < 0)
+            {
+                _sec = 59;
+                --_min;
+            }
+            _timer.text = _min + " : " + _sec;
+        }
+
+        TimerLayer.SetActive(false);
+        MainHealthBar.SetActive(false);
+        GameSetLayer.SetActive(true);
+
+        IsGameEnd = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        yield return null;
+    }
+
+    public void ViewPlayerKillList(Dictionary<string, int> KillDict)
+    {
+        var _dict = from pair in KillDict orderby pair.Value descending
+                    select pair;
+
+        int i = 0;
+        foreach (KeyValuePair<string, int> _pair in _dict)
+        {
+            PlayerRank[i].text = _pair.Key.Split('_')[0];
+            if (KillDict.Count > i) i++;
+        }
     }
 }
